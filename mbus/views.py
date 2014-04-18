13f35b0aa515372c4b4198a6480574b10c916947
Pat Pannuto <pat.pannuto@gmail.com>
2014-04-18 00:03:44 -0400

    Switch from chameleon to mako templates

--- mbus/views.py
+++ mbus/views.py
@@ -1,6 +1,6 @@
 from pyramid.view import view_config
 
 
-@view_config(route_name='home', renderer='templates/mytemplate.pt')
+@view_config(route_name='home', renderer='templates/home.mako')
 def my_view(request):
     return {'project': 'mbus'}
